fa43b5bbd6222ab5d0eeb664b8b593b2f78077b1
Aleksandar Majkic <aleksandar@comdata.rs>
2016-12-06 12:09:23 +0100

    Initing

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var PORT = 7010;
+var PORT = process.env.PORT || 8080;
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
