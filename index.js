56302a77e5e95f91135a2b4687c24399ff3c6bb8
Aleksandar Majkic <aleksandar@comdata.rs>
2016-12-10 15:45:10 +0100

    Initing

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ var allItemsFromServer = [{ "item": "★ Karambit | Gamma Doppler (Factory New)"
 var ifERROR = false;
 var refreshTime = 10000;
 var knifes = [];
-var startTime = 6;
+var startTime = 7;
 
 io.on('connection', function (socket) {
     socket.send("connect");
